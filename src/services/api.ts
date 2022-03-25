ae20ad2dda088bb844926ca55e7f3324ece4f82d
Guilherme Ferreira <guilherme.xgfx@gmail.com>
2022-03-25 10:55:22 -0300

    feat: add axios instance and api key

---
+++ src/services/api.ts
@@ -0,0 +1,9 @@
+import axios, { AxiosInstance } from "axios";
+
+const apiKey: string =
+  "6e6c9454cf6c9496a6e9e013b262578c998ba7b6dad99c3807185bc23ae1fee7";
+
+export const api: AxiosInstance = axios.create({
+  baseURL: "https://min-api.cryptocompare.com/data/",
+  headers: { authorization: apiKey },
+});
