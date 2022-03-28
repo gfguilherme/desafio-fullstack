1840f39a0bf0ddac137c0983821a9c7611cb8da4
Guilherme Ferreira <guilherme.xgfx@gmail.com>
2022-03-28 06:02:11 -0300

    feat: add project to github pages

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import "./styles/global.css";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Simulator />}></Route>
       </Routes>
